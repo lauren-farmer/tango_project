4477cab28303f9b9c191d6c6dff05e5f556f1d2d
Lauren Farmer <laurenmayafarmer@sky.com>
2024-02-09 17:35:41 +0000

    Chapter 9 Tests

--- rango/views.py
+++ rango/views.py
@@ -33,6 +33,7 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -46,6 +47,7 @@ def add_category(request):
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form':form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -110,7 +112,7 @@ def user_login(request):
         user = authenticate(username=username, password=password)
 
         if user:
-            if user.is_active():
+            if user.is_active:
                 login(request, user)
                 return redirect(reverse('rango:index'))
             else:
@@ -125,7 +127,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!") 
+    return render(request, 'rango/restricted.html') 
 
 @login_required
 def user_logout(request):
